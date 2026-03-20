68868b15a7ae793c5737efbbd365a65a31513de5
Marian <mariansousan03@gmail.com>
2026-03-20 11:31:46 -0400

    navigation update

--- Debtrix/src/app/(tabs)/_layout.tsx
+++ Debtrix/src/app/(tabs)/_layout.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// app/(tabs)/_layout.tsx
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+export default function TabLayout() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tabs>
+      <Tabs.Screen 
+        name="home" 
+        options={{ 
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }} 
+      />
+      <Tabs.Screen 
+        name="new-expense" 
+        options={{ 
+          title: 'New Expense',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add" size={50} color={color} />
+          ),
+        }} 
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Debtrix/src/components/Expenses.tsx
@@ -0,0 +1,5 @@
+import {useState, useRef, useEffect} from 'react';
+
+export default function Expenses(){
+    
+}
