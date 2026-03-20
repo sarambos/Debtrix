f147fd611aac8eca5424714ffc974c510b8f609f
Marian <mariansousan03@gmail.com>
2026-03-20 11:40:37 -0400

    merged and updated

--- Debtrix/src/app/(tabs)/home.tsx
+++ Debtrix/src/app/(tabs)/home.tsx
@@ -1,9 +1,34 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import GetTransactionHistory from '../../components/GetTransactionHistory';
+import { Transaction } from '../../types/types';
 
-export default function AboutScreen() {
+const history: Transaction[] = [
+  {
+    id: "1",
+    title: "Dinner",
+    amount: 45.50,
+    date: "2026-03-18T19:30:00Z",
+    paidBy: "Stephanie",
+    participants: ["Stephanie", "Alex"],
+    type: "paid",
+  },
+  {
+    id: "2",
+    title: "Uber",
+    amount: 18.20,
+    date: "2026-03-17T22:00:00Z",
+    paidBy: "Alex",
+    participants: ["Stephanie", "Alex"],
+    type: "received",
+  },
+]
+
+export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+      <GetTransactionHistory transactions={history}/>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -11,11 +36,8 @@ export default function AboutScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-  },
-  text: {
-    color: '#fff',
+    justifyContent: 'center',
   },
 });
